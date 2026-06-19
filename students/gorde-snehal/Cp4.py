68aad982337d8a10f3a7d4147252605697c60830
gordesnehal01-lab <gordesnehal01@gmail.com>
2026-06-19 09:59:29 +0530

    Add menu printing function in Cp4.py

---
+++ students/gorde-snehal/Cp4.py
@@ -0,0 +1,10 @@
+#Print_menu
+
+def print_menu():
+    print("===== MENU =====")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Exit")
+
+print_menu()
